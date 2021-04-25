db1aa90bfbd63b7c8476ec61c88521e17fc66e91
notabelincoln <abejordandx@gmail.com>
2021-04-25 06:15:29 +0100

    Added function prototypes in gpio_functions.h as well as necessary header files

---
+++ gpio_functions.h
@@ -0,0 +1,20 @@
+/* Abe Jordan
+ * ECE 477
+ * Project
+ * gpio_functions.h
+ * Provides function declarations for configuring gpios
+ */
+#ifndef GPIO_FUNCTIONS_H
+#define GPIO_FUNCTIONS_H
+#include "linux/gpio.h"
+#include "sys/ioctl.h"
+
+// configure GPIO as either read or write with consumer label
+int gpio_setup(unsigned int gpio_num, char *consumer_label, char gpio_mode);
+
+// read from gpio pin
+int gpio_read(struct gpiohandle_request req);
+
+// write to gpio pin
+int gpio_write(struct gpiohandle_request req, int set_value);
+#endif
